0acced6e48ded1bbe44fcafab962bfbada6484a7
unknown <bvives@iesemilidarder.com>
2024-11-20 16:27:24 +0100

    Initial

--- EXAMEN/src/examen/Main.java
+++ EXAMEN/src/examen/Main.java
@@ -1,16 +1,10 @@
 package examen;
 
-/**
- *
- * @author winadmin
- */
-public class Main {
+public class Main { 
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
+		System.out.println("Hello world !!);
+	}
     
 }
+
