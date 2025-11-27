6bf982777ec3526b8099e554be50e7febe8501ef
unknown <bvives@iesemilidarder.com>
2025-11-27 08:19:54 +0100

    PROVA

--- PROVA/src/PROVA/EX_2.java
+++ PROVA/src/PROVA/EX_2.java
@@ -4,8 +4,8 @@ import java.util.ArrayList;
 
 public class EX_2 {
     public static void main(String[] args) {
-        int[] array1;  // 'array' de 10 posicions de manera aleatòria amb números aleatoris entre 0 i 9
-        int[] array2;  // 'array' de 10 posicions de manera aleatòria amb números aleatoris entre 0 i 9
+        int[] array1;  // 'array' de 5 posicions amb valors aleatoris entre 0 i 9
+        int[] array2;  // 'array' de 5 posicions amb valors aleatoris entre 0 i 9
         ArrayList<Integer> resultat;  // 'arraylist' resultant
         
         // Crea 2 'Array' de 10 posicions amb números aleatoris entre 0 i 9
--- PROVA/src/PROVA/EX_3.java
+++ PROVA/src/PROVA/EX_3.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class EX_3 {
     public static void main(String[] args) {
-        int[] array;  // 'array' de 10 posicions de manera aleatòria amb números aleatoris entre 0 i 9
+        int[] array;  // 'array' de 5 posicions amb valors aleatoris entre 0 i 9
         ArrayList<Integer> resultat;  // 'arraylist' resultant
         
         // Genera un array de 10 posicions amb números aleatoris entre 0 i 9
